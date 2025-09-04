fd1a8cf3078769931f244fc2fdbcdff09a37ec00
Sunchy321 <sunchy321@qq.com>
2025-09-04 21:49:04 +0800

    chore: add shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     adapter: cloudflare(),
+    site:    'https://tcg.cards',
+    vite:    {
+        plugins: [tailwindcss()],
+    },
 });
